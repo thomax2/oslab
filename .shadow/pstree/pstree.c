038d39ac4b6624907514c73e8da26c5a24e16623
tracer-nju <tracer@nju.edu.cn>
2025-03-25 10:49:29 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:49:28 up 48 min,  1 user,  load average: 0.23, 0.29, 0.19

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
-  return 0;
+#define NAMELEN 32
+
+typedef struct TNode *psTree;
+struct TNode
+{
+	char name[NAMELEN];
+	int pid;
+	int level;
+	psTree parent;
+	bool is_printf;
+};
+
+int main(int argc, char *argv[])
+{
+	// for (int i = 0; i < argc; i++) {
+	//   assert(argv[i]);
+	//   printf("argv[%d] = %s\n", i, argv[i]);
+	// }
+	// assert(!argv[argc]);
+	DIR *dir;
+	dir = opendir('/proc');
+	if (dir == NULL)
+	{
+		assert(0);
+		return -1;
+	}
+
+	struct dirent *dirinfo;
+
+	while ((dirinfo = readdir(dir)) != NULL)
+	{
+		if (dirinfo->d_name[0] < '0' || dirinfo->d_name[0] > '9')
+			continue;
+		printf("%s\n",dirinfo->d_name);
+	}
+
+	closedir(dir);
+	return 0;
 }
