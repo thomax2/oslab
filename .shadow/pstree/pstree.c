eeb0362496b916e41c2bc916d52a31c8c27a21db
tracer-nju <tracer@nju.edu.cn>
2025-03-25 13:13:37 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:13:37 up  3:12,  1 user,  load average: 0.49, 0.41, 0.32

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -4,16 +4,17 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define NAMELEN 32
 
-typedef struct TNode *psTree;
+typedef struct TNode *psTNode;
 struct TNode
 {
 	char name[NAMELEN];
 	int pid;
 	int level;
-	psTree parent;
+	psTNode parent;
 	bool is_printf;
 };
 
@@ -25,7 +26,9 @@ int main(int argc, char *argv[])
 	// }
 	// assert(!argv[argc]);
 	DIR *dir;
-	dir = opendir("/proc");
+	const char basedir[] = "/proc";
+
+	dir = opendir(basedir);
 	if (dir == NULL)
 	{
 		assert(0);
@@ -36,10 +39,38 @@ int main(int argc, char *argv[])
 
 	while ((dirinfo = readdir(dir)) != NULL)
 	{
-		// if (dirinfo->d_name[0] < '0' || dirinfo->d_name[0] > '9')
-		// 	continue;
-		
-		printf("%s\t%ld\t\n",dirinfo->d_name,telldir(dir));
+		if (dirinfo->d_name[0] < '0' || dirinfo->d_name[0] > '9')
+			continue;
+		long int offset = telldir(dir);
+
+		psTNode node = (psTNode)malloc(sizeof(struct TNode));
+		char pidStatFile[30];
+		FILE *fp;
+
+		int proc_pid;
+		char proc_name[50];
+		int proc_parent;
+
+		strcpy(pidStatFile, basedir);
+		strcat(pidStatFile, "/");
+		strcat(pidStatFile, dirinfo->d_name);
+		strcat(pidStatFile, "/stat");
+
+		if ((fp = fopen(pidStatFile, "r")) == NULL)
+		{
+			printf("stat file open worng\n");
+			assert(0);
+		}
+
+		int get_num = sscanf(fp, "%d (%49[^)]) %d", &proc_pid, proc_name, &proc_parent);
+
+		if(get_num == 3)
+		{
+			printf("%d\t%s\t%d\n",proc_pid,proc_name, proc_parent);
+		}
+
+		// seekdir(dir, offset);
+		// printf("%s\t%ld\n", dirinfo->d_name, telldir(dir));
 	}
 
 	closedir(dir);
