bf79519d2298d7f1dedfe602c628c1c8f60d4d80
tracer-nju <tracer@nju.edu.cn>
2025-04-04 23:56:07 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:56:07 up 13:42,  1 user,  load average: 0.74, 0.61, 0.42

--- .shadow/libco/tests/main.c
+++ .shadow/libco/tests/main.c
@@ -17,7 +17,7 @@ static int get_count() {
 
 static void work_loop(void *arg) {
     const char *s = (const char*)arg;
-
+    printf("%s\t",currentCo->name);
     for (int i = 0; i < 100; ++i) {
         printf("%s%d  ", s, get_count());
         add_count();
