cf59459fe277ad6db1f08acf788d97b87d090bdd
tracer-nju <tracer@nju.edu.cn>
2025-04-04 10:50:56 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:50:56 up 37 min,  1 user,  load average: 0.64, 0.65, 0.56

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -338,7 +338,7 @@ void co_yield() {
             : 
             : "c"(oldCurrentCo->context.rsp),
               "b" (currentCo->stackBase),
-              "a"(coroutine_wrapper),
+              "r"(coroutine_wrapper),
               "r"(currentCo)
             : "memory"
             #else
