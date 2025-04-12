f1bd0d9792dea170242accbe07d8dd192dab051a
tracer-nju <tracer@nju.edu.cn>
2025-04-12 17:06:59 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:06:59 up  7:51,  1 user,  load average: 0.59, 0.37, 0.26

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -1,18 +1,17 @@
 #include <common.h>
-#include <threads.h>
+// #include <threads.h>
 
 enum blockStatus {
     bnon = 1,
     bused,
-    busing,
 };
 
 typedef struct blockLink{
     struct blockLink *next;
     size_t size;
-    lock_t lock;
-    enum blockStatus status;
 }blockLink_t;
+
+lock_t linkLock;
 
 size_t blockSize;
 size_t blockAllocateBit;
@@ -20,80 +19,128 @@ size_t blockAllocateBit;
 
 blockLink_t bstart,*pbend;
 size_t freeBytesRemaining;
-size_t minFreeBytesRemaining;
+size_t maxFreeBytesRemaining;
 
-// static void kinit(){
-//     size_t heapStartAddr = (size_t) heap.start;
-//     size_t heapEndAddr = (size_t) heap.end;
+static void kinit(void){
+    size_t heapStartAddr = (size_t) heap.start;
+    size_t heapEndAddr = (size_t) heap.end;
 
-//     // set two constant
-//     blockSize = sizeof(blockLink_t);
-//     blockAllocateBit = ((size_t) 1) << (sizeof(size_t)*8-1);
+    // set two constant
+    blockSize = sizeof(blockLink_t);
+    blockAllocateBit = ((size_t) 1) << (sizeof(size_t)*8-1);
 
-//     // start align upward, end align downward
-//     heapStartAddr += byteAlignment - 1;
-//     heapStartAddr &= ~(byteAlignment - 1);
+    // start align upward, end align downward
+    heapStartAddr += byteAlignment - 1;
+    heapStartAddr &= ~(byteAlignment - 1);
 
-//     heapEndAddr &= ~(byteAlignment - 1);
-//     heapEndAddr -= blockSize;
+    heapEndAddr &= ~(byteAlignment - 1);
+    heapEndAddr -= blockSize;
 
-//     // bstart ----> first heap block ----> pbend
-//     bstart.next = (blockLink_t *)heapStartAddr;
-//     bstart.size = 0;
-//     lock_init(&bstart.lock);  // assume start.lock is list's overall lock
+    // bstart ----> first heap block ----> pbend
+    bstart.next = (blockLink_t *)heapStartAddr;
+    bstart.size = 0;
+    // lock_init(&bstart.lock);  // assume start.lock is list's overall lock
+    lock_init(&linkLock);
 
-//     pbend->next = NULL;
-//     pbend->size = 0;
+    pbend->next = NULL;
+    pbend->size = 0;
 
-//     // set firstblock, and size include block struct
-//     blockLink_t *bFirstBlock = (void *)heapStartAddr;
-//     bFirstBlock->next = pbend;
-//     bFirstBlock->size = heapEndAddr - heapStartAddr;
+    // set firstblock, and size not include block struct
+    blockLink_t *bFirstBlock = (void *)heapStartAddr;
+    bFirstBlock->next = pbend;
+    bFirstBlock->size = heapEndAddr - heapStartAddr - blockSize;
 
-//     // set heapblock remain size
-//     freeBytesRemaining = bFirstBlock->size;
-//     minFreeBytesRemaining = freeBytesRemaining;
-//     return;
-// }
+    // set heapblock remain size
+    freeBytesRemaining = bFirstBlock->size;
+    maxFreeBytesRemaining = freeBytesRemaining;
+    return;
+}
+
+size_t addr_valid(blockLink_t *pblock, size_t size, size_t addrMod){
+    size_t startAddr = (size_t)pblock + blockSize;
+    if(startAddr%addrMod == 0)
+        return startAddr;
+    startAddr += blockSize;
+    size_t validAddr = startAddr;
+    validAddr += addrMod - (validAddr%addrMod);
+    if (validAddr + size <= (size_t)pblock + pblock->size)
+        return validAddr;
+    else
+        return 0;
+}
 
 static void *kalloc(size_t size) {
     
-    // // align size
-    // assert(size==0);
-    // size += blockSize;      // need a blocksize
-    // size_t trueSize = 1;
-    // while(trueSize < size)
-    //     trueSize <<= 1;
-    // size = trueSize;
-    
+    // align size
+    assert(size==0);
+    size_t addrMod = 1;
+    while(addrMod < size)
+        addrMod <<= 1;
 
-    // // get list lock to find suit block, suit= enough/nowait/
-    // blockLink_t *pblock = &bstart;
-    // lock(&bstart.lock);
-    // while (pblock != pbend){
-    //     pblock = pblock->next;
-    //     if(pblock->status == bnon && pblock->size >= size)
-    //         break;
-    // }
-    // if(pblock == pbend){
-    //     while (pblock != pbend){
-    //         pblock = pblock->next;
-    //         if(pblock->size >= size)
-    //             break;
-    //     }
-    // }
-    // assert(pblock == pbend);
-    // pblock->status = busing;
-    // unlock(&bstart.lock);
+    // get list lock to find suit block, suit= enough/nowait/
+    blockLink_t *pblock = bstart.next;
+    size_t addr=0;
+    lock(&linkLock);
+    while (pblock != pbend)
+    {
+        if(!(pblock->size & blockAllocateBit) && pblock->size >= size)    // block not allocate
+            if((addr = addr_valid(pblock, size, addrMod)) != 0) // size enough
+                break;
+        pblock = pblock->next;
+    }
+    assert(addr == 0);
+    if(addr - blockSize == (size_t)pblock)
+    {
+        blockLink_t *bNextBlock = (void *)(addr + size);
+        bNextBlock->next = pblock->next;
+        bNextBlock->size = pblock->size - size - blockSize;
 
-    // lock(&pblock->lock);
+        pblock->next = bNextBlock;
+        pblock->size = size;
+        pblock->size |= (blockAllocateBit);
+    }
+    else
+    {
+        blockLink_t *bNextBlock = (void *)(addr + size);
+        blockLink_t *bNowBlock = (void *)(addr - blockSize);
+        bNextBlock->next = pblock->next;
+        bNowBlock->next = bNextBlock;
+        pblock->next = bNowBlock;
 
+        bNextBlock->size = pblock->size - (addr - (size_t)pblock) - size - blockSize;
+        bNowBlock->size = size;
+        bNowBlock->size |= blockAllocateBit;
+        pblock->size = addr - (size_t)pblock - blockSize;
+    }
+    unlock(&linkLock);
 
-    return NULL;
+    return (void *)addr;
 }
 
 static void kfree(void *ptr) {
+    blockLink_t *pblock = (void *)((size_t)ptr - blockSize);
+    blockLink_t *ppreBlock;
+    blockLink_t *pafterBlock = pblock->next;
+    lock(&linkLock);
+    //find pre block
+    for(ppreBlock = &bstart; ppreBlock->next != pblock; ppreBlock = ppreBlock->next);
     
+    // merge pre block
+    if(!(ppreBlock->size & blockAllocateBit))
+    {
+        ppreBlock->size += pblock->size + blockSize;
+        ppreBlock->next = pblock->next;
+        pblock = ppreBlock;
+    }
+
+    // merge after block
+    if(!(pafterBlock->size & blockAllocateBit))
+    {
+        pblock->size += pafterBlock->size + blockSize;
+        pblock->next = pafterBlock->next;
+    }
+    unlock(&linkLock);
+    return;
 }
 
 static void pmm_init() {
@@ -106,6 +153,9 @@ static void pmm_init() {
         "Got %d MiB heap: [%p, %p)\n",
         pmsize >> 20, heap.start, heap.end
     );
+
+    kinit();
+
 }
 
 MODULE_DEF(pmm) = {
