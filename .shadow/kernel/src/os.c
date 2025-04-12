179337ba8e7a2a0dfbbe41da1c6515f3320e7ca3
tracer-nju <tracer@nju.edu.cn>
2025-04-12 18:07:03 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:07:03 up  8:51,  1 user,  load average: 0.84, 0.55, 0.33

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -1,5 +1,50 @@
 #include <common.h>
 
+static void test0(void)
+{
+	void *add1 = pmm->alloc(1020);
+	if (add1 == NULL)
+	{
+		printf("add is NULL\n");
+		assert(1);
+	}
+	else
+		printf("add1: %p\n", add1);
+
+	int *add1_int=(int *)add1;
+	*add1_int=9876;
+	*(add1_int+(1020-4)/4)=114514;
+	void *add2 = pmm->alloc(20);
+	if (add2 == NULL)
+	{
+		printf("add3 is NULL\n");
+		assert(1);
+	}
+	else
+		printf("add2: %p\n", add2);
+	
+	void *add3 = pmm->alloc(512);
+	if (add3 == NULL)
+	{
+		printf("add3 is NULL\n");
+		assert(1);
+	}
+	else
+		printf("add3: %p\n", add3);
+	int *add3_int=(int *)add3;
+	*add3_int=543210;
+	*(add3_int+(512-4)/4)=114514;
+	pmm->free(add2);
+	assert(*add1_int==9876);
+	assert(*(add1_int+(1020-4)/4)==114514);
+	assert(*add3_int==543210);
+	assert(*(add3_int+(512-4)/4)==114514);
+	pmm->free(add1);
+	assert(*add3_int==543210);
+	assert(*(add3_int+(512-4)/4)==114514);
+	pmm->free(add3);
+}
+
 static void os_init() {
     pmm->init();
 }
@@ -8,7 +53,14 @@ static void os_run() {
     for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
         putch(*s == '*' ? '0' + cpu_current() : *s);
     }
-    while (1) ;
+    while (1)
+    {
+        test0();
+        putch('c');
+        putch('p');
+        putch('u');
+        putch('0'+cpu_current());
+    }
 }
 
 MODULE_DEF(os) = {
