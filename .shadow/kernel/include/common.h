8b0785aacb8ebe3851e37176c20440a5482d1849
tracer-nju <tracer@nju.edu.cn>
2025-04-13 11:05:39 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:05:39 up 35 min,  1 user,  load average: 1.44, 1.43, 0.89

--- .shadow/kernel/include/common.h
+++ .shadow/kernel/include/common.h
@@ -2,3 +2,4 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include "mutex.h"
+#include "test.h"
---
+++ .shadow/kernel/include/test.h
@@ -0,0 +1,8 @@
+#ifndef TEST_H__
+#define TEST_H__
+
+void test0(void);
+void test1(void);
+void test2(void);
+
+#endif
