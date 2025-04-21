525344553475d72924edd0ade06deb1189cd69b3
tracer-nju <tracer@nju.edu.cn>
2025-04-21 21:24:01 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:24:01 up  9:06,  1 user,  load average: 0.52, 0.30, 0.29

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -68,18 +68,17 @@ int main(int argc, char *argv[]) {
                 perror("chdir failed");
                 return EXIT_FAILURE;
             }
-        
+            freopen("/dev/null","w",stdout);
             int ret = execl("/bin/sh","sh","-c","make tmp",(char *)NULL);
-            if(ret != -1)
-                printf("good\n");
         }
         else
         {
-            
+            wait();
+
         }
         
 
         // To be implemented.
-        printf("Got %zu chars.\n", strlen(line));
+        // printf("Got %zu chars.\n", strlen(line));
     }
 }
