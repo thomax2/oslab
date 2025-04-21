99d6baab5a786c73c7ca9803e90b1265fd1e3182
tracer-nju <tracer@nju.edu.cn>
2025-04-21 14:01:28 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:01:28 up  1:43,  1 user,  load average: 0.77, 0.91, 0.56

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
     static char line[4096];
+    
+    pid_t pd = fork();
 
     while (1) {
+        
         printf("crepl> ");
         fflush(stdout);
 
