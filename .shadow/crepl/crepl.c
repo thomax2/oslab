0691ef6526a29124ccd59491eb57ae596d118371
tracer-nju <tracer@nju.edu.cn>
2025-04-21 16:09:20 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:09:20 up  3:51,  1 user,  load average: 0.09, 0.22, 0.26

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -8,15 +8,16 @@ int main(int argc, char *argv[]) {
     static char line[4096];
     char cfile[] = "/tmp/test/testfile_XXXXXX";
 
-    int fd = mkstemp(cfile);
+    int tmpfd = mkstemp(cfile);
     int expressNum = 0;
 
-    if(fd == -1)
+    if(tmpfd == -1)
     {
         perror("mkstemp");
         return 1;
     }
 
+    FILE *fd = fdopen(tmpfd,'w');
     // pid_t pd = fork();
 
 
