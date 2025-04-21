2df252a13369f4efb3c450e8717218043ee5d10b
tracer-nju <tracer@nju.edu.cn>
2025-04-21 15:22:48 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:22:48 up  3:04,  1 user,  load average: 0.25, 0.25, 0.21

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     static char line[4096];
-    
-    pid_t pd = fork();
+    char cfile[] = "/tmp/test/testfile_XXXXXX";
+
+    int fd = mkstemp();
+
+    if(fd == -1)
+    {
+        perror("mkstemp");
+        return 1;
+    }
+
+    // pid_t pd = fork();
+
 
     while (1) {
         
