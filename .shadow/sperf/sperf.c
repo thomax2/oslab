22e2bb07739a1d6362349a78e9d7a28fed2fd423
tracer-nju <tracer@nju.edu.cn>
2025-05-02 01:06:36 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:06:36 up 13:50,  1 user,  load average: 0.94, 0.83, 0.68

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -98,7 +98,15 @@ int main(int argc, char *argv[], char *envp[]) {
                 // }
                 char *NameStr = (char *)malloc((size_t)pmatch[1].rm_eo - (size_t)pmatch[1].rm_so + 1);
                 memcpy(NameStr, p + pmatch[1].rm_so,(size_t)pmatch[1].rm_eo - (size_t)pmatch[1].rm_so);
-                printf("%s\n",NameStr);
+                // printf("%s\n",NameStr);
+                char *TimeStr = (char *)malloc((size_t)pmatch[1].rm_eo - (size_t)pmatch[1].rm_so + 1);
+                memcpy(TimeStr, p + pmatch[1].rm_so,(size_t)pmatch[1].rm_eo - (size_t)pmatch[1].rm_so);
+                float oneTime;
+                sscanf(TimeStr,"%f",&oneTime);
+                StringNode *newNode = (StringNode *)malloc(sizeof(StringNode));
+                newNode->str = NameStr;
+                newNode->time = oneTime;
+                sumTime += oneTime;
                 continue;
             }
             else
