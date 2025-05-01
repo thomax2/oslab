d824a322251bb006796dc174b0892cd6d9e31a76
tracer-nju <tracer@nju.edu.cn>
2025-05-02 00:50:17 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:50:17 up 13:34,  1 user,  load average: 0.73, 0.50, 0.51

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -4,13 +4,19 @@
 #include <string.h>
 #include <assert.h>
 #include <sys/wait.h>
+#include <regex.h>
+
+typedef struct StringNode {
+    char *str;
+    float time;
+    struct StringNode *next;
+} StringNode;
+
 
 
 // fork process -> strace target
 // pipe strace.stdout > main.stdio
 // main parse info
-
-
 int main(int argc, char *argv[], char *envp[]) {
     // for (int i = 0; i < argc; i++) {
     //     assert(argv[i]);
@@ -19,7 +25,7 @@ int main(int argc, char *argv[], char *envp[]) {
     // assert(!argv[argc]);
     char str[200];
 
-    char *exec_argv[] = {"strace","-t",argv[1],NULL};
+    char *exec_argv[] = {"strace","-T",argv[1],NULL};
     // char *exec_envp[] = {}
 
     // pipe 
@@ -42,11 +48,66 @@ int main(int argc, char *argv[], char *envp[]) {
     }
     else
     {
+        float sumTime = 0.0;
+        char regerrbuf[256];
+        regex_t reg;
+        const char* pattern = "^(.*?)\\s*\\([^)]*\\)\\s*=\\s*[^<]*<([^>]+)>$";
+        const size_t matchsz = 3;
+
+        // file way open pipe port
         close(pipefd[1]);
         FILE *fd = fdopen(pipefd[0],"r");
+
+        // 
+        int c = regcomp(&reg, pattern, REG_EXTENDED);
+        if (0 != c)
+        {
+            perror("reg error\n");
+            exit(1);
+        }
+        StringNode *head = (StringNode *)malloc(sizeof(StringNode));
+        assert(head!=NULL);
+
+
         while (fgets(str,sizeof(str),fd))
         {
-            printf("%s\n",str);
+            // printf("%s\n",str);
+            regmatch_t pmatch[3];
+            int matchcount = 0;
+
+            const char* p = str;
+            c = regexec(&reg, p, matchsz, pmatch, 0);
+            if (REG_NOMATCH == c)
+            {
+                /** 没有找到匹配结束循环 */
+                printf("MATCH FINISHED\n");
+                regfree(&reg);
+                // break;
+            }
+            else if (0 == c)
+            {
+                /** 找到匹配,则输出匹配到的所有捕获组(catch group) */
+                // printf("%d MATCH (%d-%d)\n", ++matchcount, pmatch[0].rm_so, pmatch[0].rm_eo);
+                // for (int i = 1; i < matchsz; ++i)
+                // {
+                //     printf("group %d :<<", i);
+                //     print_str(p, pmatch[i].rm_so, pmatch[i].rm_eo);
+                //     printf(">>\n");
+                // }
+                char *NameStr = (char *)malloc((size_t)pmatch[1].rm_eo - (size_t)pmatch[1].rm_so + 1);
+                memcpy(NameStr, p + pmatch[1].rm_so,(size_t)pmatch[1].rm_eo - (size_t)pmatch[1].rm_so);
+                printf("%s\n",NameStr);
+                continue;
+            }
+            else
+            {
+                regerror(c, &reg, regerrbuf, sizeof(regerrbuf));
+                regerrbuf[sizeof(regerrbuf) - 1] = '\0';
+                printf("%s\n", regerrbuf);
+                // break;
+                assert(1);
+            }
+
         }
     }
 
---
+++ .shadow/test/testreg.c
@@ -0,0 +1,79 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <regex.h>
+
+void print_str(const char* input, size_t _start, size_t _end)
+{
+	if (input)
+	{
+		for (size_t i = _start; i < _end; ++i)
+		{
+			printf("%c", input[i]);
+		}
+	}
+}
+
+
+int main(viod) {
+
+    const char* inputstr = "mmap(0x7f1a2b1ef000, 69632, PROT_READ|PROT_EXEC, MAP_PRIVATE|MAP_FIXED|MAP_DENYWRITE, 3, 0x6000) = 0x7f1a2b1ef000 <0.000010>";
+
+    char regerrbuf[256];
+	regex_t reg;
+
+    const char* pattern = "^(.*?)\\s*\\([^)]*\\)\\s*=\\s*[^<]*<([^>]+)>$";
+
+    int c = regcomp(&reg, pattern, REG_EXTENDED);
+    if (0 != c)
+    {
+        perror("reg error\n");
+        exit(1);
+    }
+
+    const size_t matchsz = 3;
+	regmatch_t pmatch[3];
+	/** 起始匹配的偏移量 */
+	size_t offset = 0;
+	/** 捕获计数         */
+	int matchcount = 0;
+
+    while(1)
+    {
+        const char* p = inputstr + offset;
+        c = regexec(&reg, p, matchsz, pmatch, 0);
+        if (REG_NOMATCH == c)
+        {
+            /** 没有找到匹配结束循环 */
+            printf("MATCH FINISHED\n");
+            break;
+        }
+        else if (0 == c)
+		{
+			/** 找到匹配,则输出匹配到的所有捕获组(catch group) */
+			printf("%d MATCH (%d-%d)\n", ++matchcount, pmatch[0].rm_so, pmatch[0].rm_eo);
+			for (int i = 0; i < matchsz; ++i)
+			{
+				printf("group %d :<<", i);
+				print_str(p, pmatch[i].rm_so, pmatch[i].rm_eo);
+				printf(">>\n");
+			}
+			offset += pmatch[0].rm_eo;
+			continue;
+		}
+		else
+		{
+			/************************************************************************/
+			/** regexec 调用出错输出错误信息,结束循环                               */
+			/************************************************************************/
+			regerror(c, &reg, regerrbuf, sizeof(regerrbuf));
+			regerrbuf[sizeof(regerrbuf) - 1] = '\0';
+			printf("%s\n", regerrbuf);
+			break;
+		}
+    }
+    printf("%d MATCH FOUND\n", matchcount);
+    regfree(&reg);
+	return 0;
+
+}
