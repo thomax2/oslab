14a7cb55df6920808027b07e97d99dbb426b72ec
tracer-nju <tracer@nju.edu.cn>
2025-05-02 01:10:44 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:10:44 up 13:54,  1 user,  load average: 0.47, 0.61, 0.62

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -97,14 +97,14 @@ int main(int argc, char *argv[], char *envp[]) {
                 char *NameStr = (char *)malloc((size_t)pmatch[1].rm_eo - (size_t)pmatch[1].rm_so + 1);
                 memcpy(NameStr, p + pmatch[1].rm_so,(size_t)pmatch[1].rm_eo - (size_t)pmatch[1].rm_so);
                 printf("%s\n",NameStr);
-                // char *TimeStr = (char *)malloc((size_t)pmatch[2].rm_eo - (size_t)pmatch[2].rm_so + 1);
-                // memcpy(TimeStr, p + pmatch[2].rm_so,(size_t)pmatch[2].rm_eo - (size_t)pmatch[2].rm_so);
-                // float oneTime;
-                // sscanf(TimeStr,"%f",&oneTime);
-                // StringNode *newNode = (StringNode *)malloc(sizeof(StringNode));
-                // newNode->name = NameStr;
-                // newNode->time = oneTime;
-                // sumTime += oneTime;
+                char *TimeStr = (char *)malloc((size_t)pmatch[2].rm_eo - (size_t)pmatch[2].rm_so + 1);
+                memcpy(TimeStr, p + pmatch[2].rm_so,(size_t)pmatch[2].rm_eo - (size_t)pmatch[2].rm_so);
+                float oneTime;
+                sscanf(TimeStr,"%f",&oneTime);
+                StringNode *newNode = (StringNode *)malloc(sizeof(StringNode));
+                newNode->name = NameStr;
+                newNode->time = oneTime;
+                sumTime += oneTime;
                 continue;
             }
             else
