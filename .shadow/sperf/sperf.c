c17702088cedc2fac377919d915102b58e91318e
tracer-nju <tracer@nju.edu.cn>
2025-05-02 14:01:21 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:01:21 up  3:21,  1 user,  load average: 0.16, 0.10, 0.18

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -54,11 +54,11 @@ void free_list(StringNode *head)
 // pipe strace.stdout > main.stdio
 // main parse info
 int main(int argc, char *argv[], char *envp[]) {
-    for (int i = 0; i < argc; i++) {
-        assert(argv[i]);
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
-    assert(!argv[argc]);
+    // for (int i = 0; i < argc; i++) {
+    //     assert(argv[i]);
+    //     printf("argv[%d] = %s\n", i, argv[i]);
+    // }
+    // assert(!argv[argc]);
     char str[200];
 
     // char *exec_argv[] = {"strace","-T",argv[1],NULL};
@@ -68,10 +68,6 @@ int main(int argc, char *argv[], char *envp[]) {
     exec_argv[argc+1] = NULL;
     for (size_t i = 0; i < argc-1; i++)
         exec_argv[i+2] = argv[i+1];
-    for (int i = 0; i < argc+2; i++) {
-        // assert(argv[i]);
-        printf("argv[%d] = %s\n", i, exec_argv[i]);
-    }
 
     // char *exec_envp[] = {}
 
