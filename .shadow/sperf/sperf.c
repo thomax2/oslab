d215e9ccd9a6542f28325958547a6df881184994
tracer-nju <tracer@nju.edu.cn>
2025-05-02 12:37:12 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:37:12 up  1:57,  1 user,  load average: 0.36, 0.23, 0.17

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -130,7 +130,7 @@ int main(int argc, char *argv[], char *envp[]) {
                 char *TimeStr = (char *)malloc((size_t)pmatch[2].rm_eo - (size_t)pmatch[2].rm_so + 1);
                 memcpy(TimeStr, p + pmatch[2].rm_so,(size_t)pmatch[2].rm_eo - (size_t)pmatch[2].rm_so);
                 float oneTime;
-                sscanf(TimeStr,"%f",&oneTime);
+                // sscanf(TimeStr,"%f",&oneTime);
                 StringNode *findNode = func_find(NameStr,head);
                 if(findNode == NULL)
                 {
