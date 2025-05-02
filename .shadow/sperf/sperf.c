735300fe7ed9f95dba5b0c970ecc9d9eef75ba46
tracer-nju <tracer@nju.edu.cn>
2025-05-02 13:50:30 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:50:30 up  3:11,  1 user,  load average: 0.62, 0.33, 0.30

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -69,7 +69,7 @@ int main(int argc, char *argv[], char *envp[]) {
     for (size_t i = 0; i < argc-1; i++)
         exec_argv[i+2] = argv[i+1];
     for (int i = 0; i < argc+2; i++) {
-        assert(argv[i]);
+        // assert(argv[i]);
         printf("argv[%d] = %s\n", i, exec_argv[i]);
     }
 
