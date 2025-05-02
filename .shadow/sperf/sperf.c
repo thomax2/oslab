7cc6815d8592d6fff71a98129317baab82709855
tracer-nju <tracer@nju.edu.cn>
2025-05-02 13:51:19 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:51:19 up  3:11,  1 user,  load average: 0.53, 0.34, 0.30

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -61,17 +61,17 @@ int main(int argc, char *argv[], char *envp[]) {
     assert(!argv[argc]);
     char str[200];
 
-    // char *exec_argv[] = {"strace","-T",argv[1],NULL};
-    char **exec_argv = malloc(sizeof(char *)*(argc+2));
-    exec_argv[0] = "strace";
-    exec_argv[1] = "-T";
-    exec_argv[argc+1] = NULL;
-    for (size_t i = 0; i < argc-1; i++)
-        exec_argv[i+2] = argv[i+1];
-    for (int i = 0; i < argc+2; i++) {
-        // assert(argv[i]);
-        printf("argv[%d] = %s\n", i, exec_argv[i]);
-    }
+    char *exec_argv[] = {"strace","-T",argv[1],NULL};
+    // char **exec_argv = malloc(sizeof(char *)*(argc+2));
+    // exec_argv[0] = "strace";
+    // exec_argv[1] = "-T";
+    // exec_argv[argc+1] = NULL;
+    // for (size_t i = 0; i < argc-1; i++)
+    //     exec_argv[i+2] = argv[i+1];
+    // for (int i = 0; i < argc+2; i++) {
+    //     // assert(argv[i]);
+    //     printf("argv[%d] = %s\n", i, exec_argv[i]);
+    // }
 
     // char *exec_envp[] = {}
 
