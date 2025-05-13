eaeac74159f6ff1e8d9eeca1dc75f4da42607738
tracer-nju <tracer@nju.edu.cn>
2025-05-13 16:50:51 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:50:51 up  7:02,  1 user,  load average: 2.49, 1.34, 0.82

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -195,8 +195,9 @@ void *slab_alloc(size_t size) {
             manager_slab_area[cpu].pos[page_num] += sizeof(slab_page);
 
             next_page_ptr->next_page = NULL;
-            next_page_ptr->remain_unit_num = SLAB_SIZE/(page_ptr->size) - 1;
             printf("havettttttttttttt\n");
+
+            next_page_ptr->remain_unit_num = SLAB_SIZE/(page_ptr->size) - 1;
 
             next_page_ptr->size = slab_info[cpu].page[page_num].size;
 
