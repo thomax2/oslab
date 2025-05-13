c4cd724f4f7906e415044b394922acb3d979a55c
tracer-nju <tracer@nju.edu.cn>
2025-05-13 17:39:20 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:39:20 up  7:50,  1 user,  load average: 0.61, 0.42, 0.46

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -281,7 +281,7 @@ static void kfree(void *ptr) {
             int cpu = cpu_current();
             slab_page *page_ptr;
             size_t page_start = (size_t)((char *)ptr - ((char *)ptr - (char *)zone_ptr->start) % PAGE_SIZE);
-            printf("wwwwwwwwwwwwwwwwwwwww%p\n",slab_info[cpu].page[0].next_page->start);
+            // printf("wwwwwwwwwwwwwwwwwwwww%p\n",slab_info[cpu].page[0].next_page->start);
             for (size_t i = 0; i < SLAB_NUM; i++)
             {
                 page_ptr = &(slab_info[cpu].page[i]);
