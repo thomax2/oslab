f272dcf0410b30f301fb1809d0958a1efa44cd9d
tracer-nju <tracer@nju.edu.cn>
2025-05-13 18:10:41 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:10:41 up  8:21,  1 user,  load average: 2.15, 1.45, 0.94

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -13,7 +13,7 @@ static void os_run() {
     {
         // test0();
         test_repeated_alloc(64,64*3);
-        test_repeated_alloc(512,16*3);
+        test_repeated_alloc(512,8*3);
         test_repeated_alloc(4096,8);
         test_repeated_alloc(4096*4,8);
 		// test1();
