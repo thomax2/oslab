3e03d3dfbc53211c5cc657088860618e057d2428
tracer-nju <tracer@nju.edu.cn>
2025-05-13 17:27:08 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:27:08 up  7:38,  1 user,  load average: 0.50, 0.59, 0.56

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -200,7 +200,7 @@ void *slab_alloc(size_t size) {
             // printf("havettttttttttttt\n");
 
             next_page_ptr->size = slab_info[cpu].page[page_num].size;
-
+            printf("ggboom%p\n",new_page);
             next_page_ptr->start = (uintptr_t)new_page;
             next_page_ptr->head_free = (uintptr_t)((size_t)next_page_ptr->start + next_page_ptr->size);
 
