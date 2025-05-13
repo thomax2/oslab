373e218a5ea4320f195ff1893dafccb2d108b0ad
tracer-nju <tracer@nju.edu.cn>
2025-05-13 16:50:23 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:50:23 up  7:01,  1 user,  load average: 1.81, 1.09, 0.72

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -193,11 +193,12 @@ void *slab_alloc(size_t size) {
             slab_page *next_page_ptr = (slab_page *)manager_slab_area[cpu].pos[page_num];
             void *new_page = buddy_alloc(SLAB_SIZE);
             manager_slab_area[cpu].pos[page_num] += sizeof(slab_page);
-            printf("havettttttttttttt\n");
 
             next_page_ptr->next_page = NULL;
             next_page_ptr->remain_unit_num = SLAB_SIZE/(page_ptr->size) - 1;
             next_page_ptr->size = slab_info[cpu].page[page_num].size;
+            printf("havettttttttttttt\n");
+
             next_page_ptr->start = (uintptr_t)new_page;
             next_page_ptr->head_free = (uintptr_t)((size_t)next_page_ptr->start + next_page_ptr->size);
 
