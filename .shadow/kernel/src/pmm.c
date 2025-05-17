b462c9589b13c2137edb79cc92dc3cf59845a280
tracer-nju <tracer@nju.edu.cn>
2025-05-18 00:56:36 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:56:36 up 14:26,  1 user,  load average: 1.28, 0.79, 0.64

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -294,26 +294,31 @@ void *huge_alloc(size_t size) {
     // 
     // printf("%p\n",block_ptr + 1);
 
-    for (int i = huge_list_cnt - 1; i >= block_cnt; i--) {
-        // base[i + 1] = base[i];
-        huge_block *ablock = base + i + 1;
-        huge_block *pblock = base + i;
+    // for (int i = huge_list_cnt - 1; i >= block_cnt; i--) {
+    //     // base[i + 1] = base[i];
+    //     huge_block *ablock = base + i + 1;
+    //     huge_block *pblock = base + i;
         
-        // huge_block *ablock = (huge_block *)(huge_list_start + sizeof(huge_block)*(i+1));
-        // huge_block *pblock = (huge_block *)(huge_list_start + sizeof(huge_block)*(i));
-        ablock->end = pblock->end;
-        ablock->is_used = pblock->is_used;
-        ablock->size = pblock->size;
-        ablock->start = pblock->start;
+    //     // huge_block *ablock = (huge_block *)(huge_list_start + sizeof(huge_block)*(i+1));
+    //     // huge_block *pblock = (huge_block *)(huge_list_start + sizeof(huge_block)*(i));
+    //     ablock->end = pblock->end;
+    //     ablock->is_used = pblock->is_used;
+    //     ablock->size = pblock->size;
+    //     ablock->start = pblock->start;
+    // }
+
+    for (int i = huge_list_cnt - 1; i >= block_cnt; i--) {
+        base[i + 1] = base[i];
     }
+
+
     // printf("aagg\n");
     size_t oldsize = block_ptr->size;
     block_ptr->size = size;
     block_ptr->end = block_ptr->start + size;
     block_ptr->is_used = HUGE_USED;
-    
+
     huge_block *new_block = block_ptr + 1;
-    // printf("ggd%p\n",new_block);
     new_block->start = block_ptr->end;
     new_block->size = oldsize - size;
     new_block->is_used = HUGE_UNUSED;
