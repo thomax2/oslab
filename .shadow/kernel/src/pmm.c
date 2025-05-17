11f312bf3053b2bb51eb5ba53f6834ece06ba07b
tracer-nju <tracer@nju.edu.cn>
2025-05-17 15:32:37 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:32:37 up  5:02,  1 user,  load average: 0.80, 0.61, 0.66

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -221,6 +221,7 @@ static void *kalloc(size_t size) {
     void *addr = NULL;
 
     size = align_size(size);
+    printf("pmm size:%d",size);
     assert(size >= 64);
     if(size < SLAB_SIZE)
         addr = slab_alloc(size);
