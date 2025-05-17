58d476131cd961a3d0cf1aec1dda4ac77fe16086
tracer-nju <tracer@nju.edu.cn>
2025-05-18 01:16:45 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:16:45 up 14:46,  1 user,  load average: 0.07, 0.23, 0.50

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -442,8 +442,15 @@ static void kfree(void *ptr) {
             for (int i = block_cnt; i < (int)huge_list_cnt - 1; i++) {
                 base[i] = base[i+1];
             }
-            base[huge_list_cnt - 1].is_used = 0;
+            // base[huge_list_cnt - 1].is_used = 0;
             huge_list_cnt --;
+            if (huge_list_cnt > 0) {
+                base[huge_list_cnt].start = 0;
+                base[huge_list_cnt].end = 0;
+                base[huge_list_cnt].size = 0;
+                base[huge_list_cnt].is_used = 0;
+            }
+        
             assert(huge_list_cnt >= 0);
             // printf("hugecnt5%d  \n",huge_list_cnt);
 
@@ -460,6 +467,13 @@ static void kfree(void *ptr) {
             }
             base[huge_list_cnt - 1].is_used = 0;
             huge_list_cnt --;
+            if (huge_list_cnt > 0) {
+                base[huge_list_cnt].start = 0;
+                base[huge_list_cnt].end = 0;
+                base[huge_list_cnt].size = 0;
+                base[huge_list_cnt].is_used = 0;
+            }
+        
             assert(huge_list_cnt >= 0);
             // printf("hugecnt6%d  \n",huge_list_cnt);
         }
