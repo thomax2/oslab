2d278fd2e8c41f40d374afb7e630074db21535ad
tracer-nju <tracer@nju.edu.cn>
2025-05-17 19:54:34 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:54:34 up  9:24,  1 user,  load average: 0.49, 0.46, 0.44

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -142,8 +142,8 @@ static void kinit(void){
     for (size_t i = 0; i < BUDDY_NUM; i++)
         lock_init(&(buddy_info.zone[i].buddy_lk));
 
-    huge_block *block_ptr = (huge_block *)huge_list_start;
-    printf("%d\n", block_ptr->is_used);
+    // huge_block *block_ptr = (huge_block *)huge_list_start;
+    // printf("%d\n", block_ptr->is_used);
     lock_init(&huge_lk);
     return;
 }
@@ -270,13 +270,13 @@ void *huge_alloc(size_t size) {
         block_cnt++;
     }
     
+    assert(block_ptr->is_used == HUGE_UNUSED);
 
     for (int i = huge_list_cnt - 1; i >= block_cnt; i--) {
         base[i + 1] = base[i];
     }
     huge_list_cnt ++;
-    assert(block_ptr->is_used == HUGE_UNUSED);
-
+    
     block_ptr->size = size;
     block_ptr->end = block_ptr->start + size;
     block_ptr->is_used = HUGE_USED;
