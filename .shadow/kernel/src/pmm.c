ab8948b541498c37bdbd4b5556953eb1ee133b97
tracer-nju <tracer@nju.edu.cn>
2025-05-18 01:18:44 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:18:44 up 14:48,  1 user,  load average: 0.37, 0.31, 0.50

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -324,6 +324,7 @@ void *huge_alloc(size_t size) {
     new_block->size = oldsize - size;
     new_block->is_used = HUGE_UNUSED;
 
+    printf("block_ptr%p\n",block_ptr);
     printf("new_block%p\n",new_block);
     // assert(new_block->is_used == HUGE_UNUSED);
     huge_list_cnt ++;
