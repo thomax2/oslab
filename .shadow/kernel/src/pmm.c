5095d55c0ed758df52d70c9cd503d8fea5706681
tracer-nju <tracer@nju.edu.cn>
2025-05-17 19:28:21 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:28:21 up  8:57,  1 user,  load average: 1.28, 1.20, 0.79

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -368,8 +368,8 @@ static void kfree(void *ptr) {
         huge_block *block = (huge_block *)huge_list_start;
         huge_block *base = (huge_block *)huge_list_start;
         int block_cnt = 0;
+        printf("gggggggd:%d\n",block->is_used);
         while (block->is_used == HUGE_UNUSED || block->is_used == HUGE_USED) {
-            printf("gggggggd:%d\n",block->is_used);
             if((size_t)block->start == (size_t)ptr)
                 break;
             block = block + 1;
