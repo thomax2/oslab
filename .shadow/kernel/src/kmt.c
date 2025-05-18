4fa72a9fb01e8f33dc3b44f0fdf4745ec1da20d4
tracer-nju <tracer@nju.edu.cn>
2025-05-18 14:12:55 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:12:55 up  3:47,  1 user,  load average: 1.19, 1.06, 0.95

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -111,7 +111,7 @@ static Context *kmt_context_save(Event ev, Context *ctx)
 // no choose blocked
 static Context *kmt_schedule(Event ev, Context *ctx)
 {
-    printf("sec\n");
+    // printf("sec\n");
     kmt->spin_lock(&task_lk);
     int task_cnt = 0;
     int able_cnt = 0;
@@ -181,13 +181,13 @@ static void kmt_init(void)
     {
         if(i == 0 ) {
             task_current[i] = &idle_task;
-            (&idle_task)->status = RUNNING;
+            (&idle_task)->status = RUNNABLE;
             continue;
         }
         task_t *t = pmm->alloc(sizeof(task_t));
         kmt->create(t,"_",NULL,NULL);
         task_current[i] = t;
-        t->status = RUNNING;
+        t->status = RUNNABLE;
     }
 
     os->on_irq(INT_MIN, EVENT_NULL, kmt_context_save);
