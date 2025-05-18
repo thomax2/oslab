f261a713b54cd92b7b71522928251a0a7ffd20e3
tracer-nju <tracer@nju.edu.cn>
2025-05-18 15:31:27 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:31:27 up  5:05,  1 user,  load average: 0.83, 0.85, 0.93

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -167,6 +167,7 @@ static Context *kmt_schedule(Event ev, Context *ctx)
             if(task_lib[i]->status == RUNNABLE) {
                 c--;
                 if(c == 0) {
+                    // task_lib[i]->name)
                     task_lib[i]->status = RUNNING;
                     task_current[cpu_current()] = task_lib[i];
                     kmt->spin_unlock(&task_lk);
@@ -207,7 +208,15 @@ void idle_clean_func(void *arg)
             }
         }
         kmt->spin_unlock(&task_lk);
-        yield();
+        // yield();
+    }
+}
+
+void idle_func(void *arg)
+{
+    while (1)
+    {
+        
     }
 }
 
@@ -235,7 +244,7 @@ static void kmt_init(void)
         }
         task_t *t = pmm->alloc(sizeof(task_t));
         assert(t != NULL);
-        kmt->create(t,"_",NULL,(void *)i);
+        kmt->create(t,"_",idle_func,(void *)i);
         task_current[i] = t;
         t->status = RUNNING;
     }
