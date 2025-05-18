8293c73f5e2cea291b1389dc9dd9f9ede4db3255
tracer-nju <tracer@nju.edu.cn>
2025-05-18 17:18:05 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:18:05 up  6:52,  1 user,  load average: 3.95, 2.58, 1.93

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -227,7 +227,7 @@ static Context *kmt_schedule(Event ev, Context *ctx)
             if(task_lib[i]->status == RUNNABLE) {
                 c--;
                 if(c == 0) {
-                    printf("name:%s\n",task_lib[i]->name);
+                    // printf("name:%s\n",task_lib[i]->name);
                     task_lib[i]->status = RUNNING;
                     task_current[cpu_current()] = task_lib[i];
                     kmt->spin_unlock(&task_lk);
