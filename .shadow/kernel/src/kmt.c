4a06ce56dd1841d0a3c9f05491ffcf30984addb0
tracer-nju <tracer@nju.edu.cn>
2025-05-18 16:46:10 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:46:10 up  6:20,  1 user,  load average: 1.23, 0.98, 0.87

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -12,105 +12,105 @@ task_t *task_current[CPU_NUM_MAX];
 spinlock_t task_lk;
 spinlock_t trap_lk;
 
-int irq_dis_depth[CPU_NUM_MAX];
-bool irq_enble[CPU_NUM_MAX];
+// int irq_dis_depth[CPU_NUM_MAX];
+// bool irq_enble[CPU_NUM_MAX];
 
-void kmt_spin_init(spinlock_t *lk, const char *name)
-{
-    lk->cpu = -1;
-    lk->name = name;
-    lk->status = 0;
-    return;
-}
-
-void kmt_spin_lock(spinlock_t *lk)
-{
-    if(irq_dis_depth[cpu_current()] == 0) {
-        irq_enble[cpu_current()] = ienabled();
-        iset(false);
-    }
-    irq_dis_depth[cpu_current()] ++;
-
-    size_t x= 0;
-    while (atomic_xchg(&lk->status, 1))
-    {
-        x++;
-        assert(x < 100000000);
-    }
-    lk->cpu = cpu_current();
-
-    return;
-}
-
-void kmt_spin_unlock(spinlock_t *lk)
-{
-    assert(lk->status == 1);
-    assert(lk->cpu == cpu_current());
-    atomic_xchg(&lk->status,0);
-    irq_dis_depth[cpu_current()] --;
-    if(irq_dis_depth[cpu_current()] == 0 && irq_enble[cpu_current()]) {
-        iset(true);
-    }
-
-    return;
-}
-
-// typedef struct CPU
+// void kmt_spin_init(spinlock_t *lk, const char *name)
 // {
-//     int intena; //中断信息
-//     int noff;   //递归深度
-// } CPU;
-// CPU cpus[4];
-// static void push_off()
-// {
-//     int i = ienabled();
-//     iset(false);
-//     int c = cpu_current();
-//     if (cpus[c].noff == 0)
-//         cpus[c].intena = i;
-//     cpus[c].noff++;
+//     lk->cpu = -1;
+//     lk->name = name;
+//     lk->status = 0;
+//     return;
 // }
-// static void pop_off()
+
+// void kmt_spin_lock(spinlock_t *lk)
 // {
-//     int c = cpu_current();
-//     assert(cpus[c].noff >= 1);
-//     cpus[c].noff--;
-//     if (cpus[c].noff == 0 && cpus[c].intena == true)
+//     if(irq_dis_depth[cpu_current()] == 0) {
+//         irq_enble[cpu_current()] = ienabled();
+//         iset(false);
+//     }
+//     irq_dis_depth[cpu_current()] ++;
+
+//     size_t x= 0;
+//     while (atomic_xchg(&lk->status, 1))
 //     {
+//         x++;
+//         assert(x < 100000000);
+//     }
+//     lk->cpu = cpu_current();
+
+//     return;
+// }
+
+// void kmt_spin_unlock(spinlock_t *lk)
+// {
+//     assert(lk->status == 1);
+//     assert(lk->cpu == cpu_current());
+//     atomic_xchg(&lk->status,0);
+//     irq_dis_depth[cpu_current()] --;
+//     if(irq_dis_depth[cpu_current()] == 0 && irq_enble[cpu_current()]) {
 //         iset(true);
 //     }
+
+//     return;
 // }
-// static void kmt_spin_init(spinlock_t *lk, const char *name)
-// {
-//     lk->lock = 0;
-//     lk->cpu = -1;
-//     strcpy(lk->name, name);
-// }
-// static void kmt_spin_lock(spinlock_t *lk)
-// {
-//     while (atomic_xchg(&lk->lock, 1) != 0)
-//     {
-//         if(ienabled())
-//             yield();
-//     }
-//     for(volatile int i=0;i<10000;++i);
-//     push_off(); // disable interrupts to avoid deadlock.
-//     #ifdef delock
-//     //printf("thread %s : %s , cpu's intena:%d  \n",_current->name ,lk->name,cpus[cpu_current()].intena);
-//     //printf("thread %s : %s \n",_current->name ,lk->name);
-//     #endif
-//     lk->cpu = cpu_current();
-// }
-// static void kmt_spin_unlock(spinlock_t *lk)
-// {
-//     assert(lk->cpu == cpu_current());
-//     atomic_xchg(&lk->lock, 0);
-//     lk->cpu=-1;
-//     #ifdef delock
-//     //printf("%s  unlock\n", lk->name);
-//     #endif
-//     pop_off();
-// }
+
+typedef struct CPU
+{
+    int intena; //中断信息
+    int noff;   //递归深度
+} CPU;
+CPU cpus[4];
+static void push_off()
+{
+    int i = ienabled();
+    iset(false);
+    int c = cpu_current();
+    if (cpus[c].noff == 0)
+        cpus[c].intena = i;
+    cpus[c].noff++;
+}
+static void pop_off()
+{
+    int c = cpu_current();
+    assert(cpus[c].noff >= 1);
+    cpus[c].noff--;
+    if (cpus[c].noff == 0 && cpus[c].intena == true)
+    {
+        iset(true);
+    }
+}
+static void kmt_spin_init(spinlock_t *lk, const char *name)
+{
+    lk->lock = 0;
+    lk->cpu = -1;
+    strcpy(lk->name, name);
+}
+static void kmt_spin_lock(spinlock_t *lk)
+{
+    while (atomic_xchg(&lk->lock, 1) != 0)
+    {
+        if(ienabled())
+            yield();
+    }
+    for(volatile int i=0;i<10000;++i);
+    push_off(); // disable interrupts to avoid deadlock.
+    #ifdef delock
+    //printf("thread %s : %s , cpu's intena:%d  \n",_current->name ,lk->name,cpus[cpu_current()].intena);
+    //printf("thread %s : %s \n",_current->name ,lk->name);
+    #endif
+    lk->cpu = cpu_current();
+}
+static void kmt_spin_unlock(spinlock_t *lk)
+{
+    assert(lk->cpu == cpu_current());
+    atomic_xchg(&lk->lock, 0);
+    lk->cpu=-1;
+    #ifdef delock
+    //printf("%s  unlock\n", lk->name);
+    #endif
+    pop_off();
+}
 
 
 
