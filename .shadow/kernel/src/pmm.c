68c8a515609525e3b8e8566c6ee59f510d92385c
tracer-nju <tracer@nju.edu.cn>
2025-05-18 10:39:28 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:39:28 up 13 min,  1 user,  load average: 2.25, 1.30, 0.59

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -384,7 +384,7 @@ static void kfree(void *ptr) {
     }
     else {      // in huge
         lock(&huge_lk);
-        printf("=== BLOCK TABLE BEFORE ASSERT ===\n");
+        // printf("=== BLOCK TABLE BEFORE ASSERT ===\n");
         // // debug_dump_block_list();
         huge_block *base = (huge_block *)huge_list_start;
         // for (int i = 0; i < huge_list_cnt; i++) {
@@ -408,7 +408,7 @@ static void kfree(void *ptr) {
         assert(block->is_used == HUGE_USED);
         block->is_used = HUGE_UNUSED;
         // merge pre block
-        printf("hugecnt6 %d\n",huge_list_cnt);
+        // printf("hugecnt6 %d\n",huge_list_cnt);
         while(block_cnt > 0 && base[block_cnt-1].is_used ==HUGE_UNUSED) {
             base[block_cnt-1].size += block->size;
             base[block_cnt-1].end = block->end;
