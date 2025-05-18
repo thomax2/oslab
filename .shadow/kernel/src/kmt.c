203a21b8d1dc078429d6e0996a4dc81595b658a6
tracer-nju <tracer@nju.edu.cn>
2025-05-18 17:15:11 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:15:11 up  6:49,  1 user,  load average: 1.37, 1.37, 1.49

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -245,18 +245,18 @@ task_t idle_task;
 void idle_clean_func(void *arg)
 {
     while (1) {
-        printf("idle\n");
+        // printf("idle\n");
         kmt->spin_lock(&task_lk);
-        int task_cnt = 0;
-        for (size_t i = 0; task_cnt < tid_cnt; i++) {
+        // int task_cnt = 0;
+        for (size_t i = 0; i < TASK_NUM_MAX; i++) {
             // assert(task_lib[i] != NULL);
             if(task_lib[i] != NULL) {
-                task_cnt ++;
+                // task_cnt ++;
                 if(task_lib[i]->status == DEAD) {
                     bool flag_use = false;
                     for (size_t j = 0; j < CPU_NUM_MAX; j++)
                     {
-                        if(task_current[i] != NULL && task_current[j] == task_lib[i])
+                        if (task_current[j] != NULL && task_current[j] == task_lib[i])
                             flag_use = true;
                     }
                     if(flag_use == false) {
