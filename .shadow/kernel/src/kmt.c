cc2af3465fb07fc33059108de5cb119d91e608ed
tracer-nju <tracer@nju.edu.cn>
2025-05-19 00:07:08 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:07:08 up  1:11,  1 user,  load average: 2.00, 0.95, 0.88

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -77,7 +77,7 @@ void kmt_sem_wait(sem_t *sem)
 {
     kmt->spin_lock(&sem->lk); // 获得自旋锁
     sem->value--; // 自旋锁保证原子性
-    printf("innnnn %d %s\n",sem->value,sem->name);
+    // printf("innnnn %d %s\n",sem->value,sem->name);
     if (sem->value < 0) {
         // printf("innnn\n");
 
