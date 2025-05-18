3f0d683ed72a80df161dea662784d398fb174dbf
tracer-nju <tracer@nju.edu.cn>
2025-05-18 12:07:34 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:07:34 up  1:42,  1 user,  load average: 0.67, 1.99, 1.86

--- .shadow/abstract-machine/scripts/platform/qemu.mk
+++ .shadow/abstract-machine/scripts/platform/qemu.mk
@@ -4,6 +4,7 @@ smp        ?= 4
 LDFLAGS    += -N -Ttext-segment=0x00100000
 QEMU_FLAGS += -serial mon:stdio \
               -machine accel=tcg \
+			  -S -s \
 			  -smp "$(smp),cores=1,sockets=$(smp)" \
               -drive format=raw,file=$(IMAGE)
 
