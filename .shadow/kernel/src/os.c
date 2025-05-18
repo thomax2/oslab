935ce2dd21bdaad3a3046f2c45db06d0a10e31c3
tracer-nju <tracer@nju.edu.cn>
2025-05-18 14:48:18 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:48:18 up  4:22,  1 user,  load average: 1.62, 1.31, 1.06

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -81,7 +81,7 @@ static Context *os_trap(Event ev, Context *context)
     Context *ret_ctx = NULL;    
     irq_handler_list *list = &irq_table;
     int cnt = list->cnt;
-    printf("GP Fault: cause=0x%p, ref=0x%p, msg=%s\n", ev.cause, ev.ref, ev.msg);
+    printf("GP Fault: cause=0x%d, ref=0x%d, msg=%s\n", ev.cause, ev.ref, ev.msg);
     // only one handler return a context
     for (int i = 0; i < cnt; i++) {
         irq_handler h = list->handlers[i];
