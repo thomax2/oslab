3ddbd37200407a20af8b7ae494fb1e070290aeec
tracer-nju <tracer@nju.edu.cn>
2025-05-18 18:40:39 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:40:39 up  8:15,  1 user,  load average: 0.88, 2.15, 3.43

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -128,16 +128,13 @@ void kmt_sem_wait(sem_t *sem)
     kmt->spin_lock(&sem->lk); // 获得自旋锁
     sem->value--; // 自旋锁保证原子性
     if (sem->value < 0) {
-        // 没有资源，需要等待
-        sem->queue[sem->queue_cnt] = task_current[cpu_current()];
-        task_current[cpu_current()]->status = BLOCKED;
-        // mark_as_not_runnable(current); // 当前线程不能再执行
-    }
-
-    kmt->spin_unlock(&sem->lk);
-    if (sem->value < 0) {    // 如果 P 失败，不能继续执行
-                        // (注意此时可能有线程执行 V 操作)
-        yield();        // 引发一次上下文切换
+        task_t *curr = task_current[cpu_current()];
+        sem->queue[sem->queue_cnt++] = curr;
+        curr->status = BLOCKED;
+        kmt->spin_unlock(&sem->lk);
+        yield();  // 必须立即 yield，不能继续执行
+    } else {
+        kmt->spin_unlock(&sem->lk);
     }
 }
 
