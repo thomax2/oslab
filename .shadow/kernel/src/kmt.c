6ab91b4176fa4052fa8ae70d3d08816396797e9a
tracer-nju <tracer@nju.edu.cn>
2025-05-20 23:47:08 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:47:08 up  8:18,  1 user,  load average: 2.87, 1.78, 1.17

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -188,10 +188,10 @@ void kmt_sem_signal(sem_t *sem)
         assert(sem->queue[0] != NULL);
         assert(sem->queue[0]->status == BLOCKED);
         sem->queue[0]->status = RUNNABLE;
-        printf("ssign::%s ",sem->queue[0]->name);
+        printf("ssign::%s  %s",sem->queue[0]->name,sem->name);
         // size_t i = 0;
         for (size_t i = 0; i < sem->queue_cnt - 1; i++) {
-            printf("sign::%s %s",sem->queue[i+1]->name,sem->name);
+            printf("sign::%s",sem->queue[i+1]->name);
 
             sem->queue[i] = sem->queue[i + 1];
         }
