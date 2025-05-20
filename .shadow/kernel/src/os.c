34f4c469b7c582f540762c107c5fe729d66395d1
tracer-nju <tracer@nju.edu.cn>
2025-05-20 16:28:27 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:28:27 up 59 min,  1 user,  load average: 0.87, 1.92, 2.17

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -90,7 +90,10 @@ static Context *os_trap(Event ev, Context *context)
     // only one handler return a context
     for (int i = 0; i < cnt; i++) {
         irq_handler h = list->handlers[i];
-        assert(h.handler != NULL);
+        // assert(h.handler != NULL);
+        if(h.handler == NULL) {
+            printf("waaaaaaaaaaaaa\n");
+        }
         // printf("")
         if(h.event == EVENT_NULL || h.event == ev.event) {
             Context *r = h.handler(ev, context);
