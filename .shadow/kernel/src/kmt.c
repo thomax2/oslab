12c37dbff74af2f1a377e703d9ba545d16a1c16c
tracer-nju <tracer@nju.edu.cn>
2025-05-20 19:34:46 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:34:46 up  4:05,  1 user,  load average: 1.03, 0.93, 1.14

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -32,8 +32,9 @@ void kmt_spin_lock(spinlock_t *lk)
         iset(false);
     }
     irq_dis_depth[cpu_current()] ++;
-    // printf("LOCK status:%d cpu:%d curr:%d name:%s\n", 
-    //     lk->status, lk->cpu, cpu_current(), lk->name);
+
+    printf("[LOCK ] Trying to acquire lock '%s' on CPU #%d (irq_dis_depth=%d)\n",
+           lk->name, cpu_current(), irq_dis_depth[cpu_current()]);
  
     size_t x= 0;
     while (atomic_xchg(&lk->status, 1))
@@ -61,9 +62,12 @@ void kmt_spin_unlock(spinlock_t *lk)
     assert(lk->cpu == cpu_current());
     atomic_xchg(&lk->status,0);
     irq_dis_depth[cpu_current()] --;
-    // printf("LOCK status:%d cpu:%d curr:%d name:%s\n", 
-    //     lk->status, lk->cpu, cpu_current(), lk->name);
+    printf("LOCK status:%d cpu:%d curr:%d name:%s\n", 
+        lk->status, lk->cpu, cpu_current(), lk->name);
  
+    printf("[UNLCK] Released lock '%s' on CPU #%d (irq_dis_depth=%d)\n",
+        lk->name, cpu_current(), irq_dis_depth[cpu_current()]);
+
     if(irq_dis_depth[cpu_current()] == 0 && irq_enble[cpu_current()]) {
         iset(true);
     }
