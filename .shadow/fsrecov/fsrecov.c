8089e7da79b2e288c0db0d512a07ab6a41a1091c
tracer-nju <tracer@nju.edu.cn>
2025-05-26 11:01:31 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:01:31 up  1:41,  1 user,  load average: 0.54, 0.45, 0.39

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -96,7 +96,7 @@ int main(int argc, char *argv[]) {
 
     size_t clus_num = data_size / (hdr->BPB_SecPerClus * hdr->BPB_BytsPerSec);
 
-    printf("%p\n",(FirstDataSector ) * hdr->BPB_BytsPerSec);
+    printf("%d\n",(FirstDataSector ) * hdr->BPB_BytsPerSec);
     // for (size_t i = 0; i < clus_num; i++) {
     //     u8 *addr = (u8 *)hdr + (FirstDataSector + i * hdr->BPB_SecPerClus) * hdr->BPB_BytsPerSec;
     //     int type = cluster_classify(addr, hdr->BPB_SecPerClus * hdr->BPB_BytsPerSec);
