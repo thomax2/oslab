5eebc17fe2787701c8d297cfb7740020da62a349
tracer-nju <tracer@nju.edu.cn>
2025-05-30 14:00:34 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:00:34 up  3:51,  1 user,  load average: 0.82, 0.77, 0.61

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -90,13 +90,14 @@ int cluster_classify(u8 *data, size_t size, u32 cluster_num)
     for(int i = 0; i < size; i += 32) {
 
         struct fat32dent *dir_entry = (struct fat32dent *)(data + i);
-        printf("dir_entry:%x\n",dir_entry->DIR_Name[0]);
+        printf("dir_entry:%x DIR_Attr %x\n",dir_entry->DIR_Name[0],dir_entry->DIR_Attr);
         printf("ggg %x\n",dir_entry->DIR_Name[0] & 0xF0);
         if (dir_entry->DIR_Name[0] == 0x00 ||
             dir_entry->DIR_Name[0] == 0xe5 ||
             dir_entry->DIR_Attr & ATTR_HIDDEN)
             continue;
-        if ( (dir_entry->DIR_Name[0] & 0xF0 ) == 0x40 && dir_entry->DIR_Attr == 0x0F) { // is long name file
+        if ( ((dir_entry->DIR_Name[0] & 0xF0 ) == 0x40) && (dir_entry->DIR_Attr == 0x0F)) { // is long name file
+            printf("hhh\n");
             bmp_count ++;
             int long_name_num = dir_entry->DIR_Name[0] & 0x0F;
             i += long_name_num * 32;
