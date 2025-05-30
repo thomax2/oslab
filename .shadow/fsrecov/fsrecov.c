11644a163d7c459c0270d29aa6c7287f47ac7ab5
tracer-nju <tracer@nju.edu.cn>
2025-05-30 23:11:03 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:11:03 up  3:00,  1 user,  load average: 0.38, 0.18, 0.16

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -105,7 +105,7 @@ int cluster_classify(u8 *data, size_t size, u32 cluster_num)
             // cluster_id - 2
             int bmp_num = short_dir_entry->DIR_FstClusLO;
             // printf("bmp_num: %d\n",bmp_num);
-            graph->clusters[bmp_num].bmp_info.file_size = short_dir_entry->DIR_FileSize;
+            // graph->clusters[bmp_num].bmp_info.file_size = short_dir_entry->DIR_FileSize;
             // struct fat32dent *long_dir_entry = dir_entry;
             int len = 0;
             bool flag = false;
@@ -142,7 +142,7 @@ int cluster_classify(u8 *data, size_t size, u32 cluster_num)
                 }
             }
             graph->clusters[bmp_num].bmp_info.name[len] = '\0';
-            graph->clusters[bmp_num].bmp_info.file_size = dir_entry->DIR_FileSize;
+            // graph->clusters[bmp_num].bmp_info.file_size = dir_entry->DIR_FileSize;
         }
         // printf("77777\n");
         if( i > 32*4 && bmp_count == 0) // not dirct
