193eabc5a6a9a8cabe96471b18c97cf0ec2c1a58
tracer-nju <tracer@nju.edu.cn>
2025-05-30 22:12:17 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:12:16 up  2:02,  1 user,  load average: 0.04, 0.03, 0.02

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -126,7 +126,9 @@ int cluster_classify(u8 *data, size_t size, u32 cluster_num)
             assert( len < 50 );
             graph->clusters[bmp_num].bmp_info.name[len] = '\0';
         }
-        else if( dir_entry->DIR_Attr == 0x20 && dir_entry->DIR_NTRes == 0 ) {  // is short name file
+        else if( dir_entry->DIR_Attr == 0x20 && dir_entry->DIR_NTRes == 0 &&
+             ((dir_entry->DIR_Name[0] >= 0x30 && dir_entry->DIR_Name[0] <= 0x39) ||
+             (dir_entry->DIR_Name[0] >= 0x41 && dir_entry->DIR_Name[0] <= 0x5A)) ) {  // is short name file
             bmp_count ++;
             int bmp_num = (dir_entry->DIR_FstClusHI << 16) | dir_entry->DIR_FstClusLO;
             int len = 0;
