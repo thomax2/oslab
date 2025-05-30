ebf42c468f9542a6b2c6c002d1404850b07af00c
tracer-nju <tracer@nju.edu.cn>
2025-05-30 13:51:51 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:51:51 up  3:42,  1 user,  load average: 0.98, 0.70, 0.50

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -88,13 +88,14 @@ int cluster_classify(u8 *data, size_t size, u32 cluster_num)
     // dirct
     int bmp_count=0;
     for(int i = 0; i < size; i += 32) {
+        printf("hhh\n");
+
         struct fat32dent *dir_entry = (struct fat32dent *)(data + i);
         if (dir_entry->DIR_Name[0] == 0x00 ||
             dir_entry->DIR_Name[0] == 0xe5 ||
             dir_entry->DIR_Attr & ATTR_HIDDEN)
             continue;
         if ( (dir_entry->DIR_Name[0] & 0xF0 ) == 0x40 && dir_entry->DIR_Attr == 0x0F) { // is long name file
-            printf("hhh\n");
             bmp_count ++;
             int long_name_num = dir_entry->DIR_Name[0] & 0x0F;
             i += long_name_num * 32;
