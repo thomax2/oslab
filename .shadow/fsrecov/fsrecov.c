f91c575d7bc90364f3420356cd3737d90d0baa27
tracer-nju <tracer@nju.edu.cn>
2025-05-30 13:53:35 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:53:35 up  3:44,  1 user,  load average: 0.68, 0.68, 0.51

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -90,7 +90,7 @@ int cluster_classify(u8 *data, size_t size, u32 cluster_num)
     for(int i = 0; i < size; i += 32) {
 
         struct fat32dent *dir_entry = (struct fat32dent *)(data + i);
-        printf("dir_entry:%x\n",dir_entry);
+        printf("dir_entry:%p\n",dir_entry);
         if (dir_entry->DIR_Name[0] == 0x00 ||
             dir_entry->DIR_Name[0] == 0xe5 ||
             dir_entry->DIR_Attr & ATTR_HIDDEN)
