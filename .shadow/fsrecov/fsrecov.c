5f32816b5717a83b4056e628c2625b4ca7f0d6a0
tracer-nju <tracer@nju.edu.cn>
2025-05-31 23:21:16 +0800

    Linux thomax-VirtualBox 5.15.0-134-generic #145~20.04.1-Ubuntu SMP Mon Feb 17 13:27:16 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:21:16 up 11:38,  1 user,  load average: 1.07, 0.79, 0.64

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -190,7 +190,7 @@ double get_prob(u32 source_node, u32 target_node, u32 width) {
     }
 
     double avg_diff = diff_sum / (width * 3);
-    return exp(-10 * avg_diff);
+    return exp(-1 * avg_diff);
 }
 
 
